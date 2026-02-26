33024f3b849de5f20ab51e27dcc135fa6a0a1646
KANNAN-SITE <kannan2007priyashiju@gmail.com>
2026-02-26 23:21:43 +0530

    Initial portfolio project

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const path= require("path");
-const cors = require("cors")
+const cors = require("cors");
 require("dotenv").config();
 
 const { createClient } = require("@supabase/supabase-js");
@@ -8,32 +7,41 @@ const { createClient } = require("@supabase/supabase-js");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"public")));
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
 
-
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));  
+// POST route
 app.post("/contact", async (req, res) => {
+  console.log("FORM DATA:", req.body);
+
   const { name, email, message } = req.body;
 
   const { data, error } = await supabase
     .from("contacts")
     .insert([{ name, email, message }]);
 
-  if (error) return res.status(500).json(error);
+  if (error) {
+    console.log("SUPABASE ERROR:", error.message);
+    return res.status(500).json(error);
+  }
 
   res.json({ success: true });
 });
 
 
-
+// test route
 app.get("/", (req, res) => {
   res.send("Server is working 🚀");
 });
 
 
+// server start
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
